130795689f143b152a3e50fdaca29717224e01d1
Julian Johannesen <julian.johannesen@gmail.com>
2018-04-20 20:29:07 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,10 @@
 def fizzbuzz(number)
   if number % 3 == 0 && number % 5 == 0
-    puts "Fizzbuzz"
+    "Fizzbuzz"
   elsif number % 3 == 0
-    puts "Fizz"
+    "Fizz"
   elsif number % 5 == 0
-    puts "Buzz"
+    "Buzz"
   else
     nil
   end
